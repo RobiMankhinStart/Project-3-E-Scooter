39f43fe880c14fdc1babefdafb96b347b30ee421
RobiMankhinStart <mankhinrobi@gmail.com>
2025-04-21 23:25:50 +0600

    gallery part done. working on colors and testimonials

--- js/script.js
+++ js/script.js
@@ -5,3 +5,29 @@ $(".counter").counterUp({
   delay: 10,
   time: 1000,
 });
+// .......slick slider..........
+
+$(".slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".slider-nav",
+});
+$(".slider-nav").slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: ".slider-for",
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  arrows: false,
+});
+$(".center").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: "60px",
+  slidesToShow: 3,
+  arrows: true,
+});
