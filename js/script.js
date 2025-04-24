ee43998f512906a96e6bfacd7b4c7f426fde3d11
RobiMankhinStart <mankhinrobi@gmail.com>
2025-04-25 00:32:54 +0600

    project completed with responsive

--- js/script.js
+++ js/script.js
@@ -13,6 +13,41 @@ $(".slider-for").slick({
   arrows: false,
   fade: true,
   asNavFor: ".slider-nav",
+  responsive: [
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: true,
+        prevArrow: '<i class="fa-solid arr color_prevArr fa-arrow-left"></i>',
+        nextArrow: '<i class="fa-solid arr color_nextArr fa-arrow-right"></i>',
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: true,
+        prevArrow: '<i class="fa-solid arr color_prevArr fa-arrow-left"></i>',
+        nextArrow: '<i class="fa-solid arr color_nextArr fa-arrow-right"></i>',
+      },
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: true,
+        prevArrow: '<i class="fa-solid arr color_prevArr fa-arrow-left"></i>',
+        nextArrow: '<i class="fa-solid arr color_nextArr fa-arrow-right"></i>',
+      },
+    },
+  ],
 });
 $(".slider-nav").slick({
   slidesToShow: 4,
@@ -24,12 +59,49 @@ $(".slider-nav").slick({
   arrows: false,
 });
 $(".center").slick({
-  slidesToShow: 1,
   slidesToScroll: 1,
   centerMode: true,
   centerPadding: "60px",
   slidesToShow: 3,
-  //   arrows: true,
   prevArrow: '<i class="fa-solid arr prevArr fa-arrow-left"></i>',
   nextArrow: '<i class="fa-solid arr nextArr fa-arrow-right"></i>',
+  responsive: [
+    {
+      breakpoint: 575,
+      settings: {
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 1199,
+      settings: {
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+  ],
 });
